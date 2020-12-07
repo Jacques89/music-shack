7ef76f8c39e0479764f9f321b92ecab6736e19dc
Jacques Nalletamby <jacquesnalletamby@gmail.com>
2020-12-07 20:59:10 +0100

    :wq

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useEffect ,useState } from 'react'
+import SpotifyWebApi from 'spotify-web-api-js'
+
 import './App.css'
 import Login from './components/Login/Login'
+import { getTokenFromUrl } from './spotify'
+
+const spotify = new SpotifyWebApi()
 
 const App = () => {
+  const [token, setToken] = useState()
+
+  useEffect(() => {
+    const hash = getTokenFromUrl()
+      window.location.hash = ''
+      const _token = hash.access_token
+    
+      if (_token) {
+        setToken(_token)
+        spotify.setAccessToken(_token)
+      }
+
+      console.log('token', token)
+  }, [])
+
   return (
     <div className="App">
-      <Login />
+      {token ? <h1>Logged in</h1> : <Login />}
     </div>
   )
 }
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import './Login.scss'
+import { loginUrl } from '../../spotify'
 
 const Login = () => {
   return(
     <div className='login'>
-
+      <img 
+        src='https://music-b26f.kxcdn.com/wp-content/uploads/2017/06/635963274692858859903160895_spotify-logo-horizontal-black.jpg'
+        alt='Spotify-logo'
+      />
+      <a href={loginUrl}>LOGIN WITH SPOTIFY</a>
     </div>
   )
 }
